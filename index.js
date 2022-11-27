113fbd25e6e31ca82c45f744033443cb68b9a0b1
Sara Le <116413640+sjlelait@users.noreply.github.com>
2022-11-27 01:00:46 +0000

    "completed step C in Loops"

--- index.js
+++ index.js
@@ -87,11 +87,20 @@ for(let i = 10; i <= 400; i++) {
 for(let i = 12; i <= 4000; i+=3) {
   console.log(i);
 };
-*/
 for(let i = 1; i <=100; i++) {
   if(i % 2 == 0) {
     console.log(`${i} <--is an even number`);
   } else {
     console.log(i);
+  };
+};
+*/
+for(let i = 1; i <= 100; i++) {
+  if(i % 5 == 0 && i % 3 == 0) {
+    console.log(`I found a ${i}. High five, three's a crowd!`);
+  } else if(i % 5 == 0) {
+    console.log(`I found a ${i}. High five!`);
+  } else if(i % 3 == 0) {
+    console.log(`I found a ${i}. Three's a crowd`);
   };
 };
